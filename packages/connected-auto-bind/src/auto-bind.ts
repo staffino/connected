926f8b1fa6568fad625d961fef7a662310caf6e6
Martin Komara <martin.komara@gmail.com>
2020-02-20 15:41:49 +0100

    updated autoBind to traverse over prototype own properties

--- packages/connected-auto-bind/src/auto-bind.ts
+++ packages/connected-auto-bind/src/auto-bind.ts
@@ -1,15 +1,15 @@
 export default function autoBind<T extends any>(instance: T) {
   const prototype = Object.getPrototypeOf(instance);
-  for (const i in prototype) {
-    if (prototype.hasOwnProperty(i)) {
-      const existingMethod: any = instance[i];
-      if (typeof existingMethod === 'function') {
-        const method = existingMethod.bind(instance);
-        if (existingMethod.hasOwnProperty('meta')) {
-          Object.defineProperty(method, 'meta', { value: existingMethod.meta });
-        }
-        instance[i] = method;
+  const properties = Object.getOwnPropertyNames(prototype);
+  for (let i = 0; i < properties.length; i += 1) {
+    const property = properties[i];
+    const existingMethod = instance[property];
+    if (property !== 'constructor' && typeof existingMethod === 'function') {
+      const method = existingMethod.bind(instance);
+      if (existingMethod.hasOwnProperty('meta')) {
+        Object.defineProperty(method, 'meta', { value: existingMethod.meta });
       }
+      instance[property] = method;
     }
   }
 }
