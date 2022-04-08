6f4d11be397e20f749e1065a2d4ecff177de90a4
Martin Komara <martin@staffino.com>
2022-04-08 14:11:25 +0200

    fixed compilation errors

--- packages/connected-server/src/executor/resolve-files.ts
+++ packages/connected-server/src/executor/resolve-files.ts
@@ -1,15 +1,19 @@
 import glob, { IOptions } from 'glob';
 import * as path from 'path';
 
-export default function resolveFiles(pattern: string, options: IOptions): Promise<string[]> {
-  return new Promise((resolve, reject) =>
+export default function resolveFiles(
+  pattern: string,
+  options: IOptions
+): Promise<string[]> {
+  return new Promise((resolve, reject) => {
     glob(pattern, options, (err, matches) => {
       if (err) {
         reject(err);
       } else {
-        resolve(matches.map(match => path.join(options.root!, match)));
+        resolve(matches.map((match) => path.join(options.root!, match)));
       }
-    }));
+    });
+  });
 }
 
 export type ResolveFiles = typeof resolveFiles;
